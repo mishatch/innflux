a1c36a790e5553c8ed4c8e08c6076003468584b6
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-11-17 21:06:34 +0400

    fix navbar error

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,11 +1,12 @@
 "use strict";
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav-list");
-
+const body = document.querySelector("body");
 window.onload = function () {
   menu_btn.addEventListener("click", function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
+    body.classList.toggle("no-scroll");
   });
 };
 const closeMenu = function () {
